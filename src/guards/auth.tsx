6bb255e3f9ff5861b1b778b9b0b8e8a616c3f124
Jesus Pineda <jesuspineda18@outlook.es>
2023-12-28 16:31:49 -0500

    Refactor authentication guard

--- src/guards/auth.tsx
+++ src/guards/auth.tsx
@@ -1,6 +1,4 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const GuardedRoute = ({children}: any) => {
@@ -8,11 +6,13 @@ const GuardedRoute = ({children}: any) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const isAuthenticated = useSelector((state: any) =>state.auth.isAuth);
   console.log(isAuthenticated);
-  if (isAuthenticated == "true") {
-    return children;
-  } else {
-    return <Navigate to='/' />;
-  }
+  // if (isAuthenticated == "true") {
+  //   return children;
+  // } else {
+  //   return <Navigate to='/' />;
+  // }
+
+  return children
 };
 
 export default GuardedRoute;
