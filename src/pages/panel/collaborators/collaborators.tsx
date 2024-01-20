155a14c1c6c868d5f7091300294fecf92e856724
Jesus Pineda <jesuspineda18@outlook.es>
2024-01-20 18:14:58 -0500

    Update collaborator image paths

--- src/pages/panel/collaborators/collaborators.tsx
+++ src/pages/panel/collaborators/collaborators.tsx
@@ -9,22 +9,22 @@ export default function Collaborators() {
   const [collaboratorActive, setCollaboratorActive] = useState(0);
   const collaborators = [
     {
-      img: "assets/images/collaborator4.webp",
+      img: "/assets/images/collaborator4.webp",
     },
     {
-      img: "assets/images/collaborator2.jpg",
+      img: "/assets/images/collaborator2.jpg",
     },
     {
-      img: "assets/images/collaborator1.webp",
+      img: "/assets/images/collaborator1.webp",
     },
     {
-      img: "assets/images/collaborator3.jpg",
+      img: "/assets/images/collaborator3.jpg",
     },
     {
-      img: "assets/images/collaborator5.jpeg",
+      img: "/assets/images/collaborator5.jpeg",
     },
     {
-      img: "assets/images/collaborator6.jpeg",
+      img: "/assets/images/collaborator6.jpeg",
     },
   ];
 
