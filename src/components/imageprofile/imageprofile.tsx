8290f93dcc7ebb35f56ec40a5afea610fa65a605
Jesus Pineda <jesuspineda18@outlook.es>
2024-01-21 02:39:13 -0500

    Update image source in ImageProfile component

--- src/components/imageprofile/imageprofile.tsx
+++ src/components/imageprofile/imageprofile.tsx
@@ -1,8 +1,8 @@
 import img from '../../assets/images/collaborator4.webp'
-console.log(img)
+
 export default function ImageProfile() {
   return (
     
-      <img src={'/src/assets/images/collaborator4.webp'} alt="" className='w-8 h-8 rounded-full -ml-3' />
+      <img src={img} alt="" className='w-8 h-8 rounded-full -ml-3' />
   )
 }
