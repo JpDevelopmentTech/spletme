bbc36cea81a74ea1bc22a48503f179566a6359ce
Jesus Pineda <jesuspineda18@outlook.es>
2024-10-01 23:54:31 -0500

    Refactor select dropdown buttons in home page

--- src/pages/panel/home/home.tsx
+++ src/pages/panel/home/home.tsx
@@ -212,8 +212,6 @@ export default function Home() {
 
             <span className="sr-only">Icon description</span>
           </button>
-
-      
         </div>
         <div className="col-span-2">
           <Breadcrumb />
@@ -222,8 +220,8 @@ export default function Home() {
           className="duration-200 hover:scale-105 col-span-12 lg:col-span-6 shadow-lg rounded-2xl row-span-2  p-8 flex flex-col"
           id="chart"
         >
-          <span className="text-title font-bold">Estadisticas</span>
-          <span className=" text-septenary text-subtitle">
+          <span className="font-semibold text-xl">Estadisticas</span>
+          <span className=" text-septenary text-sm">
             Reporte de tu rendimiento general
           </span>
           <ReactApexChart
@@ -233,50 +231,93 @@ export default function Home() {
             series={series}
           />
           <div className="w-full flex justify-around p-3">
-            <div className="bg-senary p-1  rounded-3xl text-septenary flex gap-2 items-center">
-              <img src={StreamLogo} alt="" className="w-3 h-3" />
-              <span className=" text-normal">325K Streams</span>
-            </div>
-            <div className="bg-senary p-1  rounded-3xl text-septenary flex gap-2 items-center">
-              <img src={YoutubeIcon} alt="" className="w-3 h-3" />
-              <span className=" text-normal">325K Streams</span>
-            </div>
-            <div className="bg-senary p-1  rounded-3xl text-septenary flex gap-2 items-center">
-              <img src={MoneyIcon} alt="" className="w-3 h-3" />
-              <span className=" text-normal">244,19</span>
-            </div>
+            <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-full me-2 dark:bg-gray-700 dark:text-gray-400 border  ">
+              <svg
+                className="w-3 h-3 me-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M15 6.037c0-1.724-1.978-2.665-3.28-1.562L7.638 7.933H6c-1.105 0-2 .91-2 2.034v4.066c0 1.123.895 2.034 2 2.034h1.638l4.082 3.458c1.302 1.104 3.28.162 3.28-1.562V6.037Z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M16.786 7.658a.988.988 0 0 1 1.414-.014A6.135 6.135 0 0 1 20 12c0 1.662-.655 3.17-1.715 4.27a.989.989 0 0 1-1.414.014 1.029 1.029 0 0 1-.014-1.437A4.085 4.085 0 0 0 18 12a4.085 4.085 0 0 0-1.2-2.904 1.029 1.029 0 0 1-.014-1.438Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              325k Streams
+            </span>
+            <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-full me-2 dark:bg-gray-700 dark:text-gray-400 border  ">
+              <svg
+                className="w-3 h-3 me-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              325K streams
+            </span>
+            <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-full me-2 dark:bg-gray-700 dark:text-gray-400 border  ">
+              <svg
+                className="w-3 h-3 me-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
+                />
+              </svg>
+              244,19
+            </span>
           </div>
         </div>
         <div className="col-span-12  lg:col-span-3 shadow-lg row-span-2 rounded-2xl  flex items-start p-8 justify-between duration-200 hover:scale-105">
           <div className="flex flex-col w-full h-full justify-start gap-12">
             <div className="flex justify-between items-center">
               <div className="flex flex-col">
-                <span className="font-bold text-title">Balance general</span>
-                <span className="text-septenary text-subtitle">3 De junio</span>
+                <span className="font-semibold text-xl">Balance general</span>
+                <span className="text-septenary ">3 De junio</span>
               </div>
               <img src={monedaNaranjaIcon} alt="" className="w-10" />
             </div>
             <div className="flex flex-col gap-3">
               <div className="flex gap-3">
                 <img src={ingresoIcon} alt="" className="w-5" />
-                <span className="font-bold text-subtitle">Ingresos</span>
-                <span className="text-[#31bd3f] text-subtitle">$00,00</span>
+                <span className="font-bold ">Ingresos</span>
+                <span className="text-[#31bd3f] ">$00,00</span>
               </div>
               <div className="flex gap-3">
                 <img src={egresoIcon} alt="" className="w-5" />
-                <span className="font-bold text-subtitle">Egresos</span>
-                <span className="text-[#FB8500] text-subtitle">$00,00</span>
+                <span className="font-bold ">Egresos</span>
+                <span className="text-[#FB8500] ">$00,00</span>
               </div>
             </div>
           </div>
         </div>
         <div className="col-span-12 lg:col-span-3 shadow-lg rounded-2xl items-center  lg:row-span-3 p-8 flex flex-col duration-200 hover:scale-105">
-          <span className="text-left text-title font-bold">
+          <span className="text-semibold text-xl font-bold">
             Mis plataformas
           </span>
-          <span className="text-septenary text-subtitle">
-            Comportamiento por plataforma
-          </span>
+          <span className="text-septenary ">Comportamiento por plataforma</span>
           <div className="flex justify-center items-center flex-col gap-6 h-full">
             <ReactApexChart
               options={options2}
@@ -368,7 +409,7 @@ export default function Home() {
               </Link>
             </div>
             <div className="flex flex-col items-center">
-              <span className="text-title font-bold">1250</span>
+              <span className="semibold text-xl font-bold">1250</span>
               <span className="text-septenary text-normal">
                 Streams + Views
               </span>
@@ -378,24 +419,24 @@ export default function Home() {
         {/* <div className="col-span-12 lg:col-span-3 shadow-lg rounded-3xl p-8 flex flex-col justify-between items-start duration-200 hover:scale-105">
           <div className="flex justify-between w-full items-start">
             <div className="flex flex-col">
-              <span className="font-bold text-title">En tu Ewallet</span>
+              <span className="font-semibold text-xl">En tu Ewallet</span>
             </div>
             <img src={checkIcon} alt="" className="w-10" />
           </div>
 
-          <span className="text-septenary text-title font-bold">$00,00</span>
+          <span className="text-septesemibold text-xl font-bold">$00,00</span>
         </div> */}
 
         <div className="col-span-12 lg:col-span-9 flex flex-col ">
           <div className="flex justify-between items-center">
             <div className="flex flex-col">
-              <span className="font-bold text-title">Top songs</span>
-              <span className="text-septenary text-subtitle">
+              <span className="font-semibold text-xl">Top songs</span>
+              <span className="text-septenary ">
                 Aqui tus mejores canciones
               </span>
             </div>
             <div>
-              <Link to={"#"} className="font-bold text-subtitle">
+              <Link to={"#"} className="font-bold ">
                 Ver todas
               </Link>
             </div>
