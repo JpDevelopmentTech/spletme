9bf8cf3b5c1d10c3bae6a14dc6953567a0ee75ba
Jesus Pineda <jpdev@outlook.es>
2024-10-15 15:04:50 -0500

    Refactorizar diseño de botón en el componente Button

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -21,7 +21,7 @@ const Button = ({
       type="button"
       onClick={onClick}
       className={
-        `font-medium  rounded-full text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 text-white ${buttonStyles[type]}`
+        `font-medium  rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 text-white ${buttonStyles[type]}`
       }
     >
       {children}
