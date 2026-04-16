6974259c6041015df4aa86fa536c7fbf71eef5eb
SantiagoAlvPer <santiago.alvarezpereira@unicolombo.edu.co>
2026-04-15 22:11:26 -0500

    The country information was added to the settings section, and a new field was added to display the country name

---
+++ src/hooks/useConvertCountry.ts
@@ -0,0 +1,20 @@
+//hook para convertir el codigo de pais a su nombre completo
+const converseCountry = (country: string) => {
+
+    switch (country.toLocaleUpperCase()) {
+        case 'CO': return 'Colombia';
+        case 'US': return 'United States';
+        case 'MX': return 'Mexico';
+        case 'AR': return 'Argentina';
+        case 'BR': return 'Brazil';
+        case 'CL': return 'Chile';
+        case 'PE': return 'Peru';
+        case 'VE': return 'Venezuela';
+        case 'EC': return 'Ecuador';
+        case 'UY': return 'Uruguay';
+        default: return country; 
+    }
+
+}
+
+export default converseCountry;
