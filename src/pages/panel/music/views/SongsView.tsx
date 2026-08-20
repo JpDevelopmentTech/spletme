3e0a552029ad6f6199abab2bb8d707aa06a4a99d
JP Development <seo@jpdev.com.co>
2026-08-20 18:57:19 -0500

    feat(songs): update grid layout in SongsView for enhanced display
    - Modified the grid layout in SongsView to increase the number of columns from 4 to 6 for larger screens, improving the visual presentation of song cards.
    - This change aims to enhance user experience by providing a more spacious and organized view of songs in grid format.

--- src/pages/panel/music/views/SongsView.tsx
+++ src/pages/panel/music/views/SongsView.tsx
@@ -164,7 +164,7 @@ export function SongsView({
   if (layout === "grid") {
     return (
       <div className="flex flex-col gap-5">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-6 xl:grid-cols-6">
           {filteredSongs.map((song) => (
             <SongCard key={song._id} song={song} onQuickView={library.handleOpenSongDetails} />
           ))}
